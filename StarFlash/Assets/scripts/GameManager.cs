9c272d2b1ef02f5bf7369e65ca78be08b0056cfa
junyuan pang <pjunyuan@vt.edu>
2019-08-30 12:20:19 -0400

    second modified

---
+++ StarFlash/Assets/scripts/GameManager.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Ez.Pooly;
+
+public class GameManager : MonoBehaviour
+{
+
+    public static GameManager instance;
+    public int level;
+    public Transform blockparent;
+
+
+    void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        }
+    }
+
+    public void GameStart()
+    {
+        Debug.Log("Game Start");
+    }
+
+
+    public void GameOver()
+    {
+        Debug.Log("Game Over");
+    }
+
+    public void CreateLevel()
+    {
+
+    }
+
+    private void Start()
+    {
+        Pooly.Spawn("block", new Vector3(0, 4, 0), Quaternion.identity );
+    }
+
+
+
+
+
+}
