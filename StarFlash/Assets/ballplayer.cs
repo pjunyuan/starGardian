3c545dd4017a96fd605726dc36073c798ca8d8ba
junyuan pang <pjunyuan@vt.edu>
2019-08-29 23:12:52 -0400

    second work finish

---
+++ StarFlash/Assets/ballplayer.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ballplayer : MonoBehaviour {
+
+    public float force;
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            Vector3 pz = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            Vector2 mousePos = new Vector2(pz.x, pz.y);
+            Vector2 ballPos = transform.position;
+            
+
+            GetComponent<Rigidbody2D>().AddForce((mousePos - ballPos).normalized * force, ForceMode2D.Force);
+        }
+    }
+
+
+    void destroyball()
+    {
+        if (Input.GetKeyDown("space"))
+        {
+            GetComponent<MeshExploder>().Explode();
+            Destroy(gameObject);
+        }
+
+    }
+
+
+
+
+}
---
+++ StarFlash/Assets/block.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class block : MonoBehaviour {
+
+
+    public int life = 99;
+    public Text lifetext;
+    public MeshExploder script;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        lifetext.text = life.ToString();
+        if (life == 0)
+        {
+            script.Explode();
+            Destroy(gameObject);
+        }
+	}
+
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "ball" && life > 0)
+        {
+            life--;
+        }
+        
+        
+    }
+
+
+
+
+
+}
